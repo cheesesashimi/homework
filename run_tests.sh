545fdfb07821149108fbfca2667861c8ff4ccf52
Zack Zlotnik <zackzlotnik@gmail.com>
2016-03-23 18:30:58 -0400

    Made test output a bit more verbose

--- run_tests.sh
+++ run_tests.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-./q2/q2_test.py
-./q4/q4_test.py
-./q5/q5_test.py
+echo "Testing Q2:";
+./q2/q2_test.py;
+
+echo "Testing Q4:";
+./q4/q4_test.py;
+
+echo "Testing Q5:";
+./q5/q5_test.py;
