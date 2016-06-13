a9c7fd46630dfb5490b624c30fc359d391c5c73d
Zack Zlotnik <zackzlotnik@gmail.com>
2016-06-13 10:44:41 -0400

    Use test auto-discovery and make tests more verbose

--- run_tests.sh
+++ run_tests.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 
-echo "Testing Q2:";
-./q2/q2_test.py;
-
-echo "Testing Q4:";
-./q4/q4_test.py;
-
-echo "Testing Q5:";
-./q5/q5_test.py;
+python -m unittest discover -v -s ./ -p '*_test.py'
